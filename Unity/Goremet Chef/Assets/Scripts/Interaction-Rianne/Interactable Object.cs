b2c75bcdda8a0fb05772593bec46e4e282628cdb
RHerrera <152225484+RHerrera30@users.noreply.github.com>
2025-04-12 22:53:47 -0700

    Made loot drop randomly from sources
    Added an array of loot prefabs so that items can drop random loot. Original code for dedicated spawns is still included just in case.

--- Unity/Goremet Chef/Assets/Scripts/Interaction-Rianne/Interactable Object.cs
+++ Unity/Goremet Chef/Assets/Scripts/Interaction-Rianne/Interactable Object.cs
@@ -5,11 +5,15 @@ public class InteractableObject : MonoBehaviour
     //This is the script that should be on any item a player can hit. Garbage cans, dumpsters, civilians.
     //Interactable objects can take damage from player and drop loot.
 
-    //The loot dropped by the object and where it spawns.
-    public GameObject dropPrefab;
+    //This array holds a list of possible item drops which can be updated through the inspector.
+    public GameObject[] possibleDrops;
+    //This is where the item is gonna spawn.
     public Transform dropOrigin;
-
-    //This is when the object is hit.
+    
+    //ONLY USE IF WE WANT A SPECIFIC ITEM SPAWN EVERY TIME. Accompanying code is still included in the Die() method.
+    // public GameObject dropPrefab;
+    
+    //This is when the object is hit by the player.
     public void TakeDamage()
     {
         //Hit!
@@ -23,11 +27,26 @@ public class InteractableObject : MonoBehaviour
     private void Die()
     {
         Debug.Log(gameObject.name + " destroyed.");
+
         
-        //Drop the item.
-        //This is to check if the drop origin for the item exists, and on the off-chance it doesn't, it'll just spawn at the object's origin.
-        Vector3 spawnPosition = dropOrigin.position != null ? dropOrigin.position : transform.position;
-        Instantiate(dropPrefab, spawnPosition, Quaternion.identity);
+        if (possibleDrops.Length > 0)
+        {
+            //This will select an item drop randomly from an array of possible drop prefabs.
+            int index = Random.Range(0, possibleDrops.Length);
+            GameObject selectedDrop = possibleDrops[index];
+            
+            //This is to check if the drop origin for the item exists, and on the off-chance it doesn't, it'll just spawn at the object's origin.
+            Vector3 spawnPosition = dropOrigin.position != null ? dropOrigin.position : transform.position;
+            
+            //I grant you existence, item drop. Use it wisely...
+            Instantiate(selectedDrop, spawnPosition, Quaternion.identity);
+        } 
+        
+        //--------THIS CODE CAN BE USED IF WE WANT TO SPAWN A SPECIFIC ITEM DROP EVERY TIME. But I thought the random was more fun.--------//
+            //This is to check if the drop origin for the item exists, and on the off-chance it doesn't, it'll just spawn at the object's origin.
+            // Vector3 spawnPosition = dropOrigin.position != null ? dropOrigin.position : transform.position;
+            // Instantiate(dropPrefab, spawnPosition, Quaternion.identity);
+        //----------------------------------------------------------------------------------------------------------------------------------//
         
         //Get rid of the original object.
         Destroy(gameObject);
