20cba8d226bb3c5e06358260e36ca43f1643c0c5
Michael Garcia <157657140+Generic-Mann@users.noreply.github.com>
2025-04-14 19:01:17 -0700

    Finished player jump feature
    - jump force is public, so you can freely adjust how high the player can jump.

--- Unity/Goremet Chef/Assets/Scripts/PlayerMove-Michael/PlayerMovement.cs
+++ Unity/Goremet Chef/Assets/Scripts/PlayerMove-Michael/PlayerMovement.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // character controller - Michael
-    // Base simple character controller created by Rianne (given permission)
+    // simple character controller created by Rianne used as base (given permission)
     
     private CharacterController controller;
-
+    private bool isGrounded;
+    
     public float speed = 5f;
+    public float jump = 5f;
+    private float gravity = -9.8f;
+    private float groundGravity = -0.05f;
+    private float verticalVelocity = 0f;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -15,7 +21,28 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        isGrounded = controller.isGrounded;
+
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * (Time.deltaTime * speed));
+
+        if (isGrounded) 
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jump;
+            }
+            else
+            {
+                verticalVelocity = groundGravity;
+            }
+        } 
+        else 
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+        
+        move.y = verticalVelocity;
+        controller.Move(move * (jump * Time.deltaTime));
     }
 }
