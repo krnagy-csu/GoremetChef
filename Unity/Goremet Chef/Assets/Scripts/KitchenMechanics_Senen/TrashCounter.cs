53c3b49c5f2461b831b31283c6fd8fac156db471
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2025-04-20 23:35:28 -0700

    Added Countertop Point
    Changed delivery and trash counters to extend BaseCounter. Added countertop points.

--- Unity/Goremet Chef/Assets/Scripts/KitchenMechanics_Senen/TrashCounter.cs
+++ Unity/Goremet Chef/Assets/Scripts/KitchenMechanics_Senen/TrashCounter.cs
@@ -1,14 +1,14 @@
 using System;
 using UnityEngine;
 
-public class TrashCounter : MonoBehaviour
+public class TrashCounter : BaseCounter
 {
     public static event EventHandler OnAnyObjectTrashed;
 
-    new public static void ResetStaticData()
+    /*new public static void ResetStaticData()
     {
         OnAnyObjectTrashed = null;
-    }
+    }*/
     
     /*public override void Interact(Player player) {
         if (player.HasKitchenObject()) {
