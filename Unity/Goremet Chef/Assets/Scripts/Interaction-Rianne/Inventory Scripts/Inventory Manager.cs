5a03437cb23dc895e759e3bbb815153c330c5f23
RHerrera <152225484+RHerrera30@users.noreply.github.com>
2025-04-22 00:53:23 -0700

    Difficulties with the object delete button
    For some reason I can't get the items to delete when I hit the button for them. Committing for the sake of saving my progress, will figure it out tomorrow.

--- Unity/Goremet Chef/Assets/Scripts/Interaction-Rianne/Inventory Scripts/Inventory Manager.cs
+++ Unity/Goremet Chef/Assets/Scripts/Interaction-Rianne/Inventory Scripts/Inventory Manager.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using TMPro;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
     public List<Item> items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    public Toggle EnableRemove;
+    
+    public InventoryItemController [] InventoryItems;
     private void Awake()
     {
         Instance = this;
@@ -19,5 +28,59 @@ public class InventoryManager : MonoBehaviour
     public void Remove(Item item)
     {
         items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        //Clean list before reopening inventory
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+        
+        foreach (var item in items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<TMP_Text>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            var removeButton = obj.transform.Find("Remove Button").GetComponent<Button>();
+            
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+
+            if (EnableRemove.isOn)
+            {
+                removeButton.gameObject.SetActive(true);
+            }
+        }
+        SetInventoryItems();
+    }
+
+    public void EnableItemsRemove()
+    {
+        if (EnableRemove.isOn)
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("Remove Button").gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (Transform item in ItemContent)
+            {
+                item.Find("Remove Button").gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetInventoryItems()
+    {
+        InventoryItems = ItemContent.GetComponentsInChildren<InventoryItemController>();
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            InventoryItems[i].AddItem(items[i]);
+        }
     }
 }
