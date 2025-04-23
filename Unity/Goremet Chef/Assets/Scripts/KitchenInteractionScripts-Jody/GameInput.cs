ef3631c3440c353a4dc9fb0ebe870d998c68892b
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2025-04-23 16:59:43 -0700

    Added Interactions and Counters Layer Mask
    Added interaction method to player script. Added counters layer mask to player and clear counter prefabs.

--- Unity/Goremet Chef/Assets/Scripts/KitchenInteractionScripts-Jody/GameInput.cs
+++ Unity/Goremet Chef/Assets/Scripts/KitchenInteractionScripts-Jody/GameInput.cs
@@ -4,11 +4,20 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
+    
     private PlayerInputActions playerInputActions;
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalized() {
--- Unity/Goremet Chef/Assets/Scripts/KitchenInteractionScripts-Jody/Player.cs
+++ Unity/Goremet Chef/Assets/Scripts/KitchenInteractionScripts-Jody/Player.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -6,6 +9,13 @@ public class Player : MonoBehaviour
     
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
+    [SerializeField] private Transform kitchenObjectHoldPoint;
+    
+    private bool isWalking;
+    private Vector3 lastInteractDir;
+    private BaseCounter selectedCounter;
+    private Item item;
     
     private void Awake() {
         if (Instance != null) {
@@ -14,8 +24,22 @@ public class Player : MonoBehaviour
         Instance = this;
     }
     
+    private void Start() {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+        //gameInput.OnInteractAlternateAction += GameInput_OnInteractAlternateAction;
+    }
+    
     private void Update() {
         HandleMovement();
+        HandleInteractions();
+    }
+    
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        //if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
+        /*if (selectedCounter != null) {
+            selectedCounter.Interact(this);
+        }*/
     }
     
     private void HandleMovement() {
@@ -42,80 +66,15 @@ public class Player : MonoBehaviour
         if (canMove) {
             transform.position += moveDir * moveDistance;
         }
-        //isWalking = moveDir != Vector3.zero;
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
-}
-
-/*using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : MonoBehaviour, IKitchenObjectParent {
-
-
-    public static Player Instance { get; private set; }
-
-
-
-    public event EventHandler OnPickedSomething;
-    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
-    public class OnSelectedCounterChangedEventArgs : EventArgs {
-        public BaseCounter selectedCounter;
-    }
-
-
-    [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private GameInput gameInput;
-    [SerializeField] private LayerMask countersLayerMask;
-    [SerializeField] private Transform kitchenObjectHoldPoint;
-
-
-    private bool isWalking;
-    private Vector3 lastInteractDir;
-    private BaseCounter selectedCounter;
-    private KitchenObject kitchenObject;
-
-
-    private void Awake() {
-        if (Instance != null) {
-            Debug.LogError("There is more than one Player instance");
-        }
-        Instance = this;
-    }
-
-    private void Start() {
-        gameInput.OnInteractAction += GameInput_OnInteractAction;
-        gameInput.OnInteractAlternateAction += GameInput_OnInteractAlternateAction;
-    }
-
-    private void GameInput_OnInteractAlternateAction(object sender, EventArgs e) {
-        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
-
-        if (selectedCounter != null) {
-            selectedCounter.InteractAlternate(this);
-        }
-    }
-
-    private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
-        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
-
-        if (selectedCounter != null) {
-            selectedCounter.Interact(this);
-        }
-    }
-
-    private void Update() {
-        HandleMovement();
-        HandleInteractions();
-    }
-
+    
     public bool IsWalking() {
         return isWalking;
     }
-
+    
     private void HandleInteractions() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
@@ -128,71 +87,39 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
         float interactDistance = 2f;
         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
             if (raycastHit.transform.TryGetComponent(out BaseCounter baseCounter)) {
-                // Has ClearCounter
                 if (baseCounter != selectedCounter) {
                     SetSelectedCounter(baseCounter);
                 }
             } else {
                 SetSelectedCounter(null);
-
             }
         } else {
             SetSelectedCounter(null);
         }
     }
-
-    private void HandleMovement() {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        float moveDistance = moveSpeed * Time.deltaTime;
-        float playerRadius = .7f;
-        float playerHeight = 2f;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
-
-        if (!canMove) {
-            // Cannot move towards moveDir
-
-            // Attempt only X movement
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = (moveDir.x < -.5f || moveDir.x > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
-
-            if (canMove) {
-                // Can move only on the X
-                moveDir = moveDirX;
-            } else {
-                // Cannot move only on the X
-
-                // Attempt only Z movement
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = (moveDir.z < -.5f || moveDir.z > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
-
-                if (canMove) {
-                    // Can move only on the Z
-                    moveDir = moveDirZ;
-                } else {
-                    // Cannot move in any direction
-                }
-            }
-        }
-
-        if (canMove) {
-            transform.position += moveDir * moveDistance;
-        }
-
-        isWalking = moveDir != Vector3.zero;
-
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-    }
-
+    
     private void SetSelectedCounter(BaseCounter selectedCounter) {
         this.selectedCounter = selectedCounter;
 
-        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+        /*OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
             selectedCounter = selectedCounter
-        });
+        });*/
+    }
+}
+
+/*
+    public event EventHandler OnPickedSomething;
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+        public BaseCounter selectedCounter;
+    }
+
+    private void GameInput_OnInteractAlternateAction(object sender, EventArgs e) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
+        if (selectedCounter != null) {
+            selectedCounter.InteractAlternate(this);
+        }
     }
 
     public Transform GetKitchenObjectFollowTransform() {
