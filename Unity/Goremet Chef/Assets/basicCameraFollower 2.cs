702186d2b32b1f02295bcf1b186d70db1a47bc1e
Krisztian Nagy <krnagy@csumb.edu>
2025-04-23 20:52:23 -0700

    Revert "fix duplicate script & test it out"
    This reverts commit 12b4f5e3bc9557fdcd67590244a6474fe46220bf.

--- Unity/Goremet Chef/Assets/basicCameraFollower 2.cs
+++ Unity/Goremet Chef/Assets/basicCameraFollower 2.cs
@@ -1,4 +1,4 @@
-/*using UnityEngine;
+using UnityEngine;
 
 public class basicCameraFollower : MonoBehaviour
 {
@@ -16,4 +16,3 @@ public class basicCameraFollower : MonoBehaviour
         transform.position = objectToFollow.position + offset;
     }
 }
-*/
