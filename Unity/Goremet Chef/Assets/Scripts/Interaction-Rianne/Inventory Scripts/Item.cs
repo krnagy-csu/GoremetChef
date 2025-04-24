976d025dbe2e60c29a655e500a612f3f8aaf4988
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2025-04-23 17:38:20 -0700

    Added KitchenObject and IKitchenObjectParent Scripts
    Created scripts for KitchenObject and IKitchenObjectParent (for counters/player). Added prefab transform to Item script (Item == KitchenObjectSO).

--- Unity/Goremet Chef/Assets/Scripts/Interaction-Rianne/Inventory Scripts/Item.cs
+++ Unity/Goremet Chef/Assets/Scripts/Interaction-Rianne/Inventory Scripts/Item.cs
@@ -9,4 +9,5 @@ public class Item : ScriptableObject
     public string itemName;
     public int weight;
     public Sprite icon;
+    public Transform prefab;
 }
--- Unity/Goremet Chef/Assets/Scripts/KitchenMechanics_Senen/ClearCounter.cs
+++ Unity/Goremet Chef/Assets/Scripts/KitchenMechanics_Senen/ClearCounter.cs
@@ -4,30 +4,45 @@ using UnityEngine;
  * this class handles picking up and putting down ONLY for the clear counter
  * anything can be placed on the clear counter, plates, body parts, ingredients
  */
-public class ClearCounter : BaseCounter {
+public class ClearCounter : BaseCounter
+{
 
-    public void Interact(PlayerKitchenInteractions player) {
+    [SerializeField] private Item itemSO;
+    [SerializeField] private Transform counterTopPoint;
+
+    private Item item;
+
+    public void Interact()
+    {
+        if (item == null)
+        {
+            Transform itemTransform = Instantiate(itemSO.prefab, counterTopPoint);
+            itemTransform.localPosition = Vector3.zero;
+        }
+    }
+
+    /*public void Interact(PlayerKitchenInteractions player) {
         if (!hasObjectOnCounter()) {
             // no object on counter
             if (!player.inventoryIsEmpty()) {
                 // player has something
-                
+
                 setThingOnCounter(player.getMostRecentItem());
-                // the players gives most recent item in their inventory to the counter 
-                
+                // the players gives most recent item in their inventory to the counter
+
                 // Instantiate(getThingOnCounter(), countTopPoint.transform.position, Quaternion.identity);
-                // ^ this is for debugging if theres something in your inventory already, the game should 
+                // ^ this is for debugging if theres something in your inventory already, the game should
                 // start you off with nothing in your inventory
-                
+
                 getThingOnCounter().transform.position = countTopPoint.transform.position;
                 getThingOnCounter().SetActive(true);
                 // puts the object that was given to that counter at the top point and shows it
-                
+
                 player.removeFromInventory();
-                // removes item from invetory 
+                // removes item from invetory
             } else {
                 Debug.Log("Nothing in your inventory");
-                //player has nothing 
+                //player has nothing
             }
         } else {
             Debug.Log("something on counter " + getThingOnCounter());
@@ -35,9 +50,9 @@ public class ClearCounter : BaseCounter {
             if (!player.inventoryHasRoom()) {
                 Debug.Log("Your inventory is full");
                 return;
-            } 
+            }
             // this section below handles getting things into your inventory from the counter
-            
+
             player.addToInventory(getThingOnCounter());
             // adds thing on counter into inventory
             getThingOnCounter().SetActive(false);
@@ -45,5 +60,5 @@ public class ClearCounter : BaseCounter {
             Debug.Log(getThingOnCounter() + " added to inventory");
             clearObjectOnCounter();
         }
-    }
+    }*/
 }
