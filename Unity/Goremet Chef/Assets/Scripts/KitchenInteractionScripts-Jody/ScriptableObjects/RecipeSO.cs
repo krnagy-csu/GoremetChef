7b29bd352032128d73f825e844b608c2855e3bdd
sbagos27 <sbagos@csumb.edu>
2025-04-29 18:41:28 -0700

    muted jodys RecipeSO

--- Unity/Goremet Chef/Assets/Scripts/KitchenInteractionScripts-Jody/ScriptableObjects/RecipeSO.cs
+++ Unity/Goremet Chef/Assets/Scripts/KitchenInteractionScripts-Jody/ScriptableObjects/RecipeSO.cs
@@ -1,12 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu()]
-public class RecipeSO : ScriptableObject {
-
-    public List<Item> kitchenObjectSOList;
-    public string recipeName;
-
-
-}
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// [CreateAssetMenu()]
+// public class RecipeSO : ScriptableObject {
+//
+//     public List<Item> kitchenObjectSOList;
+//     public string recipeName;
+//
+//
+// }
